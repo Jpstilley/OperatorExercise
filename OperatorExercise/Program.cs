ab415eb6b5576c3842ad9c2d2ec312700c98f1f8
Joey Stilley <Joey.Stilley@gmail.com>
2021-08-20 11:00:39 -0500

    Completed Operator Exercises 1 & 2

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 1
+            int addition = 2 + 2;
+            int subtraction = 4 - 3;
+            int multiplication = 2 * 3;
+
+            Console.WriteLine($"{addition} {subtraction} {multiplication}");
+
+            int a = 17;
+            int b = 4;
+
+            int division = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {division} remainder {remainder}");
+
+
+            //Exercise 2
+
+            Console.WriteLine("Please enter a whole number");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            double result = AreaOfCircle(userInput);
+
+            Console.WriteLine($"The area of your circle is {result}.");
+
+
+        }
+
+        public static double AreaOfCircle(int r)
+        {
+            double calculatedCircle = Math.PI * (r ^ 2);
+            return calculatedCircle;
+        }
+    }
+}
